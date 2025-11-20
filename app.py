72dff1cdf23be92c0703582c3baecf6bc6cba7b6
arshadhussain92128-ship-it <arshadhussain92128@gmail.com>
2025-11-20 10:25:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.title("My First Streamlit App")
-st.write("Streamlit is working successfully!")
+st.title("BMI Calculator")
+
+weight = st.number_input("Enter weight (kg)")
+height = st.number_input("Enter height (m)")
+
+if st.button("Calculate"):
+    if height > 0:
+        bmi = weight / (height ** 2)
+        st.success(f"Your BMI is {bmi:.2f}")
